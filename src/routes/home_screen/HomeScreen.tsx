7d11d54fb8b176a922c439126061887276266806
rk <radiyak@umich.edu>
2023-03-28 04:33:36 -0400

    change

--- src/routes/home_screen/HomeScreen.tsx
+++ src/routes/home_screen/HomeScreen.tsx
@@ -1,15 +1,18 @@
 import { Icon } from '@/shared-components/icon';
 import { IconButton } from '@/shared-components/icon-button';
 import React from 'react';
-import { View, Image, Alert } from 'react-native';
+import { View, Image, Alert, FlatList } from 'react-native';
 import { Text, Button } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,ScrollView,SafeAreaView } from 'react-native';
 import { ThemeColors } from '@/lib/theme';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { ScreenProps } from '../types';
 import { MainStackNavigatorParamList } from '../AppNavigator';
 import { BottomTabParamList } from '../BottomTabNavigator';
+import { HomeLocations } from './HomeLocations';
+import { useGetLocations } from '@/api/points-of-interest/getLocations';
+import { PointOfInterest } from '@/api/points-of-interest';
 
 interface HomeScreenProps
 	extends CompositeScreenProps<
@@ -18,6 +21,7 @@ interface HomeScreenProps
 	> {}
 
 const HomeScreenImages: React.FC<HomeScreenProps> = ({ navigation }) => {
+	const{data: locations, isFetching:loading}=useGetLocations();
 	return (
 		<View style={format.main}>
 			<View style={format.topbar}>
@@ -46,71 +50,47 @@ const HomeScreenImages: React.FC<HomeScreenProps> = ({ navigation }) => {
 			<Image style={format.logo} source={require('@/assets/logo.png')} />
 
 			<View style={format.line}></View>
+			<View style={format.line2}></View>
+			
+		 				<SafeAreaView style={format.s}>
+				<FlatList style={format.fl} showsVerticalScrollIndicator={false}
+				
+					data={locations}
+					
+					renderItem={({item})=>(
+					<HomeLocations info ={item}/>
+				
+				)}
+					
+					keyExtractor={(item)=>item.id}
 
-			<View style={format.grid}>
-				<View style={format.box}>
-					<View>
-						<Image
-							style={format.imageone}
-							source={{
-								uri: 'https://cdn.discordapp.com/attachments/1062450289468252306/1073368868476698724/PXL_20230209_222158970.jpg',
-							}}
-						/>
-					</View>
-					<View style={format.thinline}></View>
+					/>
 
-					<Button
-						// mode="contained"  !This fixes the button
-						onPress={() => {}}
-						style={format.button}
-					>
-						<Text style={format.buttonText}>Engineering Lab</Text>
-					</Button>
-				</View>
-				<View style={format.box}>
-					<View>
-						<Image
-							style={format.imageone}
-							source={{
-								uri: 'https://cdn.discordapp.com/attachments/1062450289468252306/1081286947101753404/PXL_20230303_184017517.jpg',
-							}}
-						/>
-					</View>
-					<View style={format.thinline}></View>
-
-					<Button
-						// mode="contained"  !This fixes the button
-						onPress={() => {}}
-						style={format.button}
-					>
-						<Text style={format.buttonText}>University Center</Text>
-					</Button>
-				</View>
-				<View style={format.box}>
-					<View>
-						<Image
-							style={format.imageone}
-							source={{
-								uri: 'https://cdn.discordapp.com/attachments/1062450289468252306/1073368870674513951/PXL_20230209_221722772.PORTRAIT.jpg',
-							}}
-						/>
-					</View>
-					<View style={format.thinline}></View>
-
-					<Button
-						// mode="contained"  !This fixes the button
-						onPress={() => {}}
-						style={format.button}
-					>
-						<Text style={format.buttonText}>Mardigian Library</Text>
-					</Button>
-				</View>
+                </SafeAreaView>
+			
+			
+				
+				
 			</View>
-		</View>
 	);
 };
 
 const format = StyleSheet.create({
+	s:{
+		flex:1,
+		marginTop: -40,
+		
+		backgroundColor:'white',
+		
+		alignItems:'center'
+		
+	},
+	fl:{
+		flex:1,
+		
+		
+		
+	},
 	main: {
 		backgroundColor: 'cornflowerblue',
 		flex: 1,
@@ -130,39 +110,23 @@ const format = StyleSheet.create({
 		marginHorizontal: 30,
 		marginVertical: 10,
 	},
-	grid: {
-		flexDirection: 'row',
-		flexWrap: 'wrap',
-		marginVertical: 40,
-		marginHorizontal: 0,
-		backgroundColor: 'ivory',
-		paddingTop: 45,
-		paddingLeft: 35,
-		paddingRight: 35,
-		height: '100%',
-		width: '100%',
-		justifyContent: 'space-between',
-	},
-	buttonText: {
-		textAlign: 'center',
-		fontSize: 11,
-		fontWeight: 'bold',
-		color: ThemeColors.secondary,
-	},
-	button: {
-		//backgroundColor: ThemeColors.secondary, // need to take out the background color to make the button ripple effect work
-		borderColor: ThemeColors.secondary,
-		borderWidth: 0.2,
-		borderRadius: 5,
-		maxHeight: 40,
-		bottom: -10,
-	},
+	
+	
 	line: {
 		backgroundColor: ThemeColors.secondary,
 		borderColor: ThemeColors.secondary,
 		borderWidth: 10,
 		// borderRadius:5,
 		marginVertical: -40,
+		
+	},
+	line2: {
+		backgroundColor: 'white',
+		borderColor: "white",
+		borderWidth: 10,
+		// borderRadius:5,
+		marginVertical: 40,
+		
 	},
 	thinline: {
 		backgroundColor: 'white',
@@ -171,31 +135,7 @@ const format = StyleSheet.create({
 		// borderRadius:5,
 		//marginVertical:-40
 	},
-	box: {
-		width: 150,
-		height: 180,
-		borderColor: ThemeColors.primary,
-		borderWidth: 5,
-		alignItems: 'center',
-		//marginVertical:200,
-		//marginHorizontal:30,
-		marginBottom: 35,
-		backgroundColor: ThemeColors.primary,
-		flexDirection: 'column',
-		borderRadius: 10,
-	},
 
-	imageone: {
-		height: 85,
-		width: 100,
-		marginVertical: 10,
-		marginBottom: 20,
-		//marginHorizontal:26,
-		borderColor: ThemeColors.secondary,
-		borderWidth: 1,
-		borderRadius: 3,
-		// resizeMode: 'contains',
-	},
 	logo: {
 		height: 180,
 		width: 250,
