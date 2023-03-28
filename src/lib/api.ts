674a653174d565080e2698a008c1132905a6e7bb
Ahoutari <ahoutari@umich.edu>
2023-03-28 19:18:17 -0400

    small fix not saved

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,28 +1,5 @@
 import axios from 'axios';
-import { useTokenStore } from './stores/tokenStore';
 
 export const api = axios.create({
 	baseURL: 'http://10.0.0.219:8000',
 });
-
-api.interceptors.request.use(
-	async (config) => {
-		let headers = config.headers;
-
-		console.log('interceptor');
-
-		const token = useTokenStore.getState().accessToken;
-
-		if (token) {
-			headers.setAuthorization(`Bearer ${token}`);
-		}
-
-		return {
-			...config,
-			headers,
-		};
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
-);
